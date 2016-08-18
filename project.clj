1b1f3925c96b8417884c362feb338b5b92959e14
Joost Diepenmaat <joost@zeekat.nl>
2016-08-18 16:46:45 +0200

    Prep v0.1.9

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject rill-event-sourcing/wheel "0.1.8"
+(defproject rill-event-sourcing/wheel "0.1.9"
   :description "Command & aggregate handling for rill"
   :url "https://github.com/rill-event-sourcing/wheel"
   :license {:name "Eclipse Public License"
